0cfb6fa2a237d57e6ef6a665a737016c8377f9ee
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2023-05-17 02:34:34 +0300

    feat: add authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
+import { useEffect } from 'react';
+import { useAppSelector } from './hooks/hooks';
 
 export const App = () => {
+  const { isAuth } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuth) navigate('/login');
+  }, [isAuth]);
+
   return (
     <>
       <Routes>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,20 +1,48 @@
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { login, setLoginData } from '../../redux/reducers/authSlice';
+import { useNavigate } from 'react-router-dom';
+
 export const Login = () => {
+  const { idInstance, apiTokenInstance, isAuth } = useAppSelector((state) => state.auth);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/');
+    }
+  }, [isAuth]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value, name } = event.target;
+    dispatch(setLoginData({ name, value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    dispatch(login());
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <h2 className="form-title">Авторизация</h2>
       <input
         type="text"
         className="input"
-        placeholder="Введите idInstance"
-        // value={idInstance}
-        // onChange={handleChange}
+        name="idInstance"
+        placeholder="Введите IdInstance"
+        value={idInstance}
+        onChange={handleChange}
       />
       <input
         type="text"
         className="input"
-        placeholder="Введите apiTokenInstance"
-        // value={apiTokenInstance}
-        // onChange={handleChange}
+        name="apiTokenInstance"
+        placeholder="Введите ApiTokenInstance"
+        value={apiTokenInstance}
+        onChange={handleChange}
       />
       <button type="submit" className="btn">
         Войти
