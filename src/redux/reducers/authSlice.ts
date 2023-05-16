570362bff2c764899386a1f42a60417c3944fc39
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2023-05-17 02:24:31 +0300

    feat: add reducers and store

---
+++ src/redux/reducers/authSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export type AuthState = {
+  idInstance: string;
+  apiTokenInstance: string;
+  isAuth: boolean;
+  [key: string]: string | boolean;
+};
+
+const initialState: AuthState = {
+  idInstance: '',
+  apiTokenInstance: '',
+  isAuth: false,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setLoginData: (
+      state,
+      action: PayloadAction<{
+        name: string;
+        value: string;
+      }>
+    ) => {
+      state[action.payload.name] = action.payload.value;
+    },
+    login: (state) => {
+      state.isAuth = true;
+    },
+  },
+});
+
+export const { setLoginData, login } = authSlice.actions;
+
+export default authSlice.reducer;
